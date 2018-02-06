a4c9df660196ab941e8133d4772afa5c12d514a1
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-02-06 17:09:26 +0800

    Init commit

---
+++ sugar-loop.lisp
@@ -0,0 +1,18 @@
+(defmacro range (end)
+  "Returns a list of numbers from 0 to end (exclusive)."
+  (let ((gs (gensym)))
+    `(loop for ,gs below ,end collect ,gs)))
+
+(defmacro repeatedly (n f)
+  "Returns a list of results when calling function f for n number of times."
+  `(loop repeat ,n collect (funcall ,f)))
+
+(defmacro partition (n coll &key step)
+  "Returns a list of lists of n items each."
+  (let ((gss (repeatedly n #'gensym)))
+    `(loop for (,@gss) on ,coll by #'(lambda (arg)
+                                       (nthcdr (or ,step ,n) arg))
+           collect (list ,@gss))))
+
+(defun contains-nil-p (coll)
+  (position nil coll))
